6ca4841d2af07437756377d23c5eb79ab281bcd3
김지훈 <James@gimjihun-ui-MacBook-Pro-2.local>
2017-03-14 14:10:20 +0900

    switch test

--- src/index.js
+++ src/index.js
@@ -40,12 +40,18 @@ app.post('/commands/starbot', (req, res) => {
   }
   
 
-  console.log("Hello world")
 
   console.log(payload)
 
-
-  let task = listCommand
+  let task
+  switch(payload.text){
+    case 'list' : 
+        task = listCommand
+        break
+    case 'help' :
+        task = helpCommand
+        break
+  }
 
   let cmd = _.reduce(commands, (a, cmd) => {
     return payload.text.match(cmd.pattern) ? cmd : a
