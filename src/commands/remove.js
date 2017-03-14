6b45c7ee4dc1574df227c2b6b66a5d3d9ae133f2
김지훈 <James@gimjihun-ui-MacBook-Pro-2.local>
2017-03-14 17:51:30 +0900

    add remove.js

---
+++ src/commands/remove.js
@@ -0,0 +1,98 @@
+'use strict'
+
+var Restaurants      = require('../models/restaurant')
+
+const _ = require('lodash')
+const config = require('../config')
+const trending = require('github-trending')
+
+const msgDefaults = {
+  response_type: 'in_channel',
+  username: 'Starbot',
+  icon_emoji: config('ICON_EMOJI')
+}
+
+const handler = (payload, res) => {
+  console.log("add handler initiated")
+
+  var team_name = payload.team_domain
+  var team_id = payload.team_id
+  var user_name = payload.user_name
+  var user_id = payload.user_id
+
+  var text = payload.text
+  var blocks = text.split(" ")
+  var restaurant_name = blocks[1]
+  console.log("restaurant name is " + restaurant_name)
+  
+  if(blocks[0]=="remove"){    
+      if(blocks.length == 2 && blocks[0]=="add"){
+          console.log("ready to remove restaurant")
+          find_restaurant(user_name, user_id, team_name, team_id, restaurant_name, function(err,find_restaurant){
+              
+              if(err){
+                  console.log(err)
+                  res.send(500)
+              }
+              else{
+                  find_restaurant.remove({ _id: req.body.id }, function(err) {
+                    if (err) {
+                     console.log("Error on remove")
+                     }
+             else {
+              
+               console.log("restaurant " + restaurant_name + " removed.")
+
+              }
+          });
+
+		  let attachments = [
+		  {
+		    title: 'Lunch Today!',
+		    color: '#2FA44F',
+		    text: "restaurant " + restaurant_name + "removed!",
+		    mrkdwn_in: ['text']
+		  }]
+		  let msg = _.defaults({
+		    channel: payload.channel_name,
+		    attachments: attachments
+		  }, msgDefaults)
+
+		  res.set('content-type', 'application/json')
+		  res.status(200).json(msg)
+
+              }
+          })           
+      }
+      else if(blocks.length < 2){
+          console.log("restaurant name not supplied")
+          res.send("restaurant name is needed! See help")
+      }
+  }
+  else{
+      console.log("not an add command")
+  }
+
+
+
+  return
+}
+
+var find_restaurant = function(user_name, user_id, team_name, team_id, restaurant_name, cb){
+  Restaurants.find({user_name: user_name,
+                  user_id: user_id,
+               	  team_name: team_name,
+          	  team_id: team_id,
+             	  restaurant_name: restaurant_name}, function(err,added_restaurant){
+	    if(err){
+                cb(err, null)
+            }
+            else{
+                console.log(added_restaurant)
+                cb(null, added_restaurant)
+            }
+	})
+}
+
+module.exports = { pattern: /remove/ig, handler: handler }
+
