639edd89d369c8f680c3ed013412b805a79b70c5
김지훈 <James@gimjihun-ui-MacBook-Pro-2.local>
2017-03-15 10:24:04 +0900

    rm all

--- src/commands/remove.js
+++ src/commands/remove.js
@@ -29,7 +29,6 @@ const handler = (payload, res) => {
   
   if(blocks[0]=="remove" && blocks.length > 1 ){    
      Restaurants.find({
-      restaurant_name: restaurant_name
      }).remove().exec(function(err, result) {
 
         if (!err) {
