7d55e7ad77abce0272f5148a62ba900d90c71f90
김지훈 <James@gimjihun-ui-MacBook-Pro-2.local>
2017-03-15 17:51:48 +0900

    add googlemap

--- src/navigation.js
+++ src/navigation.js
@@ -1,23 +1,14 @@
 
 'use strict'
 
-const dotenv = require('dotenv')
-const ENV = process.env.NODE_ENV || 'development'
+var publicConfig = {
+  key: 'AIzaSyAPKefB_eM0X050XxQLgQBrjX7OQuyP6Oc',
+  stagger_time:       1000, // for elevationPath 
+  encode_polylines:   false,
+  secure:             true, // use https 
+  proxy:              '' // optional, set a proxy for HTTP requests 
+};
+var gmAPI = new GoogleMapsAPI(publicConfig);
 
-if (ENV === 'development') dotenv.load()
 
-const config = {
-  ENV: process.env.NODE_ENV,
-  PORT: process.env.PORT,
-  PROXY_URI: process.env.PROXY_URI,
-  WEBHOOK_URL: process.env.WEBHOOK_URL,
-  STARBOT_COMMAND_TOKEN: process.env.STARBOT_COMMAND_TOKEN,
-  SLACK_TOKEN: process.env.SLACK_TOKEN,
-  ICON_EMOJI: ':stars:'
-}
-
-module.exports = (key) => {
-  if (!key) return config
-
-  return config[key]
-}
+module.exports = Navigation;
