bca62b664a9edb3b5eda3b2ff47a6103de457487
김지훈 <James@gimjihun-ui-MacBook-Pro-2.local>
2017-03-16 10:53:33 +0900

    error fix

--- src/index.js
+++ src/index.js
@@ -55,8 +55,7 @@ app.post('/commands/starbot', (req, res) => {
   
   //todo check
   //today history
-  if(payload.text == "" &&
-     11 > hours && hours > 13)
+  if(payload.text == "" && ( hours < 11 || hours > 13 ) )
   {
        res.end("죄송합니다. 영업시간이 아닙니다.")
        return
