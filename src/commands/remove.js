4ec0e16fd525afd4419d65ec23072ee3d959c8f6
김지훈 <James@gimjihun-ui-MacBook-Pro-2.local>
2017-03-20 16:48:26 +0900

    remove test

--- src/commands/remove.js
+++ src/commands/remove.js
@@ -29,7 +29,7 @@ const handler = (payload, res) => {
   
   if(blocks[0]=="remove" && blocks.length > 1 ){    
 
-    Restaurants.find({restaurant_name: /restaurant_name/i }).remove().exec(
+    Restaurants.findOne({restaurant_name: /.*restaurant_name.*/i }).remove(
       function(err, removed) {
         
       
