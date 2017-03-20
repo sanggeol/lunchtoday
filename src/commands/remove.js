98073c5577ecd60112a94237aecbce71f6779c11
김지훈 <James@gimjihun-ui-MacBook-Pro-2.local>
2017-03-20 17:11:44 +0900

    hard remove test

--- src/commands/remove.js
+++ src/commands/remove.js
@@ -35,9 +35,9 @@ const handler = (payload, res) => {
       
         var result_msg = " 이 목록에서 제거되었습니다."
         
-        if(removed.result.n == 0){
-          result_msg = "은(는) 목록에 존재하지 않습니다."
-        }
+        // if(removed.result.n == 0){
+        //   result_msg = "은(는) 목록에 존재하지 않습니다."
+        // }
         
        let attachments = [
         {
