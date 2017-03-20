1ce65ca205f26e8159a4dfcb2595ad34abd2f0fb
김지훈 <James@gimjihun-ui-MacBook-Pro-2.local>
2017-03-20 10:50:40 +0900

    test staticmap

--- src/navigation.js
+++ src/navigation.js
@@ -5,9 +5,9 @@ var request = require('request');
 
 var GoogleMapsAPI = require('googlemaps')
 
+//AIzaSyBJ23W-OJvknLM6s-TC29G6TBZENjIVgGY
 var publicConfig = {
-  key: 'AIzaSyAPKefB_eM0X050XxQLgQBrjX7OQuyP6Oc',
-  stagger_time:       1000, // for elevationPath 
+  key: 'AIzaSyBJ23W-OJvknLM6s-TC29G6TBZENjIVgGY',
   encode_polylines:   false,
   secure:             true, // use https 
   proxy:              '' // optional, set a proxy for HTTP requests 
